515f2cad8ec3478a42dc34396fdff48987ed80f4
zeyti12a <116185849+zeyti12a@users.noreply.github.com>
2023-09-10 09:10:01 -0300

    Determine o número total de livros por autor

--- listaExercicios01.sql
+++ listaExercicios01.sql
@@ -8,5 +8,9 @@ WHERE matriculas.curso = 'Engenharia de Software';
 SELECT produto, SUM(receita) AS receita_total
 FROM vendas
 GROUP BY produto;
+SELECT autores.nome, COUNT(livros.id) AS total_livros
+FROM autores
+LEFT JOIN livros ON autores.id = livros.autor_id
+GROUP BY autores.nome;
 
 
