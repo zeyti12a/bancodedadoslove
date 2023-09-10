c2977c323ebb0d9f65e578174ff79c4fc75df647
zeyti12a <116185849+zeyti12a@users.noreply.github.com>
2023-09-10 09:13:27 -0300

    Determine os autores que têm mais de 2 livros publicados.

--- listaExercicios01.sql
+++ listaExercicios01.sql
@@ -22,3 +22,8 @@ SELECT produto, SUM(receita) AS receita_total
 FROM vendas
 GROUP BY produto
 HAVING receita_total > 10000.00;
+SELECT autores.nome
+FROM autores
+LEFT JOIN livros ON autores.id = livros.autor_id
+GROUP BY autores.nome
+HAVING COUNT(livros.id) > 2;
