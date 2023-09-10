8a7d93480d3dd9ad3f26a7f5335648bd76384578
zeyti12a <116185849+zeyti12a@users.noreply.github.com>
2023-09-10 09:07:59 -0300

    Encontre todos os alunos matriculados em 'Engenharia de Software'

--- listaExercicios01.sql
+++ listaExercicios01.sql
@@ -1,4 +1,8 @@
 SELECT titulo FROM livros;
 SELECT nome FROM autores WHERE nascimento < '1900-01-01';
 SELECT titulo FROM livros WHERE autor_id = 1;
+SELECT alunos.nome
+FROM alunos
+INNER JOIN matriculas ON alunos.id = matriculas.aluno_id
+WHERE matriculas.curso = 'Engenharia de Software';
 
