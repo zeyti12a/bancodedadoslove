a495686d8614cdd976e93eeb42a48970f5d8528d
zeyti12a <116185849+zeyti12a@users.noreply.github.com>
2023-09-26 20:47:07 -0300

    Livros e Seus Autores

--- listaExercicios01.sql
+++ listaExercicios01.sql
@@ -161,7 +161,17 @@ DELIMITER ;
 
 DELIMITER //
 CREATE PROCEDURE sp_ListarAutores()
+BEGIN 
+
+DELIMITER //
+CREATE PROCEDURE sp_LivrosESeusAutores()
 BEGIN
+    SELECT Livro.Titulo, CONCAT(Autor.Nome, ' ', Autor.Sobrenome) AS Autor
+    FROM Livro
+    INNER JOIN Autor_Livro ON Livro.Livro_ID = Autor_Livro.Livro_ID
+    INNER JOIN Autor ON Autor_Livro.Autor_ID = Autor.Autor_ID;
+END //
+DELIMITER ;
     
     SELECT * FROM Autor;
 END //
