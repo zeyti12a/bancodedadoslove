64a1a72a5dd8943c025c22f1254ee9f27ce12140
zeyti12a <116185849+zeyti12a@users.noreply.github.com>
2023-09-26 20:44:18 -0300

    Extração de Títulos por Categoria

--- listaExercicios01.sql
+++ listaExercicios01.sql
@@ -120,4 +120,13 @@ BEGIN
 END //
 DELIMITER ;
 
+DELIMITER //
+CREATE PROCEDURE sp_TitulosPorCategoria(IN categoriaNome VARCHAR(100))
+BEGIN
+    SELECT Livro.Titulo
+    FROM Livro
+    INNER JOIN Categoria ON Livro.Categoria_ID = Categoria.Categoria_ID
+    WHERE Categoria.Nome = categoriaNome;
+END //
+DELIMITER ;
 
